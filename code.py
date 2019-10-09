5e898f05f4b28694821665c1be45d306d5af4d5a
Ian Simpson <mrsimpson.rgc@gmail.com>
2019-10-09 15:17:48 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,9 @@
 import random
 import time
 
+things = ['coins','chocolate','gems']
+process = ['learn','strive','commit']
+
 print("Welcome to my fortune teller")
 
 question = input("Would you like me to tell your fortune?\n")
@@ -9,7 +12,7 @@ colour = input("What is your favourite colour?\n")
 print("I will now select your fortune based on your selection of ", colour)
 time.sleep(2)
 
-fortunes = ["You win all the things",
-			"Good things will happen if you learn to code"]
+fortunes = ["You win all the "+random.choice(things),
+			"Good things will happen if you "+random.choice(process)+" to code"]
 			
 print(random.choice(fortunes))
